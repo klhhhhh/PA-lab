398427d0a754af6597a71f217d968b88fc154e71
tracer-ics2017 <tracer@njuics.org>
2022-03-23 21:59:11 +0000

    >  compile 1911433 ics Linux b784bd4be617 5.13.0-35-generic #40~20.04.1-Ubuntu SMP Mon Mar 7 09:18:32 UTC 2022 x86_64 GNU/Linux  21:59:11 up 1 day,  4:48,  2 users,  load average: 0.11, 0.07, 0.01

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -262,7 +262,7 @@ uint32_t expr(char *e, bool *success) {
   /* TODO: Insert codes to evaluate the expression. */
   // TODO();  
   //
-  //printf("RESULT=%d\n",eval(0, nr_token-1));
+  printf("RESULT=%d\n",eval(0, nr_token-1));
 
   return eval(0, nr_token-1);
 }
