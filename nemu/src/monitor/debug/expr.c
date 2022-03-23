6d9afa238f25294786b4b7907abc133b7d698e28
klhhhhh <1412841649@qq.com>
2022-03-23 19:34:55 +0000

    finish the enum of the token type

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -6,13 +6,16 @@
 #include <sys/types.h>
 #include <regex.h>
 
-enum {
-  TK_NOTYPE = 256, TK_EQ
+enum {  //start from 256 to avoid ascii
+  TK_NOTYPE = 256, TK_HEX, TK_DEC, TK_REG, TK_EQ, TK_NEQ, 
+  TK_AND, TK_OR,
+  TK_NEG,      //negative number
+  TK_POI,       //use for get the content of pointer
+  TK_LS, TK_RS, TK_BOE, TK_LOE
 
   /* TODO: Add more token types */
 
 };
-
 static struct rule {
   char *regex;
   int token_type;
@@ -25,6 +28,8 @@ static struct rule {
   {" +", TK_NOTYPE},    // spaces
   {"\\+", '+'},         // plus
   {"==", TK_EQ}         // equal
+  {"0|[1-9][0-9]*",TK_NUMBER},//number 0 and number not start with 0
+  
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
