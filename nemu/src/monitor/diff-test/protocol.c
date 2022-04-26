84e74e0584a40a2b53d54e3bc1b7a3fd5acc1352
tracer-ics2017 <tracer@njuics.org>
2022-04-26 05:34:30 +0000

    >  compile 1911433 ics Linux b784bd4be617 5.13.0-39-generic #44~20.04.1-Ubuntu SMP Thu Mar 24 16:43:35 UTC 2022 x86_64 GNU/Linux  05:34:30 up 2 days, 3 min,  0 users,  load average: 0.54, 0.30, 0.36 26c81376c397647cc3bfd9136435d0093b0406bb

--- nemu/src/monitor/diff-test/protocol.c
+++ nemu/src/monitor/diff-test/protocol.c
@@ -115,6 +115,7 @@ struct gdb_conn* gdb_begin_inet(const char *addr, uint16_t port) {
   int fd = socket(AF_INET, SOCK_STREAM, 0);
   if (fd < 0)
     err(1, "socket");
+  Log("connect");
   if (connect(fd, (const struct sockaddr *)&sa, sizeof(sa)) != 0) {
     close(fd);
     return NULL;
