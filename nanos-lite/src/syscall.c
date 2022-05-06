8fd116a592060562e1c01535e787699d1be94762
tracer-ics2017 <tracer@njuics.org>
2022-05-06 23:41:23 +0000

    >  run 1911433 ics Linux b784bd4be617 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 GNU/Linux  23:41:23 up 1 day,  6:58,  1 user,  load average: 0.17, 0.12, 0.04 4b415e99919f234cefe486da7ecced758c5b5d

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,25 +2,25 @@
 #include "syscall.h"
 
 
-static uintptr_t sys_none() {
-	return 1;
-}
+// static uintptr_t sys_none() {
+// 	return 1;
+// }
 
-void sys_exit(int status) {
-	_halt(status);
+// void sys_exit(int status) {
+// 	_halt(status);
 	
-}
+// }
 
-static uintptr_t sys_write(int fd, void *buf, size_t count) {
-    if (fd == 1 || fd == 2) { // stdout or stderr
-      size_t i;
-      for (i = 0; i < count; i++) {
-        _putc(*((char*)buf + i));
-      }  
-      return i;
-    }  
-    return -1;
-  }
+// static uintptr_t sys_write(int fd, void *buf, size_t count) {
+//     if (fd == 1 || fd == 2) { // stdout or stderr
+//       size_t i;
+//       for (i = 0; i < count; i++) {
+//         _putc(*((char*)buf + i));
+//       }  
+//       return i;
+//     }  
+//     return -1;
+//   }
 
 _RegSet* do_syscall(_RegSet *r) {
   uintptr_t a[4];
@@ -30,9 +30,9 @@ _RegSet* do_syscall(_RegSet *r) {
   a[3] = SYSCALL_ARG4(r);
 
   switch (a[0]) {
-    case SYS_none: a[0] = sys_none(); break;
-    case SYS_exit: sys_exit(a[1]); break;
-    case SYS_write: a[0] = sys_write(a[1], (void*)a[2], a[3]); break;
+    // case SYS_none: a[0] = sys_none(); break;
+    // case SYS_exit: sys_exit(a[1]); break;
+    // case SYS_write: a[0] = sys_write(a[1], (void*)a[2], a[3]); break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
