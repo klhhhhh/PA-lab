c71803d5eec74f15e78348cbb96b8f8bc79bd6e2
tracer-ics2017 <tracer@njuics.org>
2022-05-06 21:43:42 +0000

    >  compile 1911433 ics Linux b784bd4be617 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 GNU/Linux  21:43:42 up 1 day,  5:00,  1 user,  load average: 0.00, 0.02, 0.00 7b366c078ab9625c47259ee1e6f47a8138486e89

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,18 +7,23 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
    */
 
   //TODO();
-  
-  // assert(NO <= cpu.idtr.limit);
-  
-  rtl_push(&cpu.eflags.val);
-  cpu.eflags.IF = 0;
+  memcpy(&t1,&cpu.eflags,sizeof(cpu.eflags));
+  rtl_li(&t0,t1);
+  rtl_push(&t0);
   rtl_push(&cpu.cs);
-  rtl_push(&ret_addr);
-  rtl_li(&t0, vaddr_read(cpu.idtr.base + NO * 8, 2));
-  rtl_li(&t1, vaddr_read(cpu.idtr.base + NO * 8 + 6, 2));
-  memcpy((void*)&decoding.jmp_eip, (void*)&t0, 2);
-  memcpy((void*)((char*)&decoding.jmp_eip + 2), (void*)&t1, 2);
-  decoding.is_jmp = 1;
+  rtl_li(&t0,ret_addr);
+  rtl_push(&t0);
+
+  vaddr_t gate_addr=cpu.idtr.base+NO*sizeof(GateDesc);
+  assert(gate_addr<=cpu.idtr.base+cpu.idtr.limit);
+
+  uint32_t off_15_0=vaddr_read(gate_addr,2);
+  uint32_t off_32_16=vaddr_read(gate_addr+sizeof(GateDesc)-2,2);
+  uint32_t target_addr=(off_32_16<<16)+off_15_0;
+
+  decoding.is_jmp=1;
+  decoding.jmp_eip=target_addr;
+  
 }
 
 void dev_raise_intr() {
