a58ebc90affbedd423f2cd28fe3ecab2eefa279b
tracer-ics2017 <tracer@njuics.org>
2022-05-08 00:40:39 +0000

    >  run 1911433 ics Linux b784bd4be617 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 GNU/Linux  00:40:39 up  1:16,  1 user,  load average: 0.19, 0.13, 0.05 c9a5854ec06f41bc47573f41d0aebd94d8358ee8

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -2,7 +2,7 @@
 
 // #define DEFAULT_ENTRY ((void *)0x4000000)
 
-#define DEFAULT_ENTRY ((void *)0x8048000)
+#define DEFAULT_ENTRY ((void *)0x4000000)
 
 //注意函数声明
 extern void ramdisk_read(void *buf, off_t offset, size_t len);
