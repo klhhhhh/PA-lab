b600bcb1a1970d1a691c87dadcc5781a7d07c90c
tracer-ics2017 <tracer@njuics.org>
2022-05-15 07:06:44 +0000

    >  compile 1911433 ics Linux b784bd4be617 5.13.0-41-generic #46~20.04.1-Ubuntu SMP Wed Apr 20 13:16:21 UTC 2022 x86_64 GNU/Linux  07:06:44 up 15:23,  1 user,  load average: 0.34, 0.17, 0.19 5ae0704ffbaa1f886aee5eafdb0b8bce6cef2b91

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -15,10 +15,28 @@ size_t events_read(void *buf, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 void dispinfo_read(void *buf, off_t offset, size_t len) {
-  
+  memcpy(buf, dispinfo + offset, len);
 }
 
 void fb_write(const void *buf, off_t offset, size_t len) {
+  off_t x = offset / sizeof(uint32_t) % _screen.width;
+  off_t y = offset / sizeof(uint32_t) / _screen.width;
+  off_t w = len / sizeof(uint32_t);
+  if (x + w >= _screen.width) {
+  	_draw_rect(buf, x, y, _screen.width - x, 1);
+  	w -= (_screen.width - x);
+  	uint32_t *buf1 = (uint32_t*)buf + (_screen.width - x);
+  	if (w / _screen.width > 0) {
+  	  _draw_rect(buf1, 0, y + 1, _screen.width, w / _screen.width);
+  	  buf1 += (w - w % _screen.width);
+  	  w %= _screen.width;
+  	  y += (w / _screen.width);
+  	}
+  	_draw_rect(buf1, 0, y + 1, w, 1);
+  }
+  else {
+	_draw_rect(buf, x, y, w, 1);
+  }
 }
 
 void init_device() {
@@ -26,4 +44,6 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+  sprintf(dispinfo, "WIDTH: %d\nHEIGHT: %d", _screen.width, _screen.height);
+  Log("WIDTH: %d HEIGHT: %d", _screen.width, _screen.height);
 }
